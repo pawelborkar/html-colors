b02ce732ed13387d701877ca908cebaa1064709d
Pawel Borkar <pawelsb@zohomail.com>
2022-03-29 23:12:39 +0530

    Complete Project: HTML Color Input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useState } from "react"
+import DisplayBox from "./components/DisplayBox"
+import Footer from "./components/Footer"
+import SeachBox from "./components/SeachBox"
 
 const App = () => {
+  const [search, setSearch] = useState('')
+
+
   return (
     <div className='App'>
-        <h1>Hello World</h1>
+      <DisplayBox 
+        search = {search}
+      />
+      <SeachBox
+        search={search}
+        setSearch={setSearch}
+      />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>Made with 💖 by Pawel</footer>
+    )
+}
+
+export default Footer
